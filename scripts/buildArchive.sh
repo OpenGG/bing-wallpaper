62881ec5437276f435b38569ccbc01f69dfa10b7
OpenGG <liy099@gmail.com>
2023-03-21 05:53:08 +0000

    feat: move archive files

--- scripts/buildArchive.sh
+++ scripts/buildArchive.sh
@@ -10,11 +10,11 @@ cd -
 
 mkdir -p archive
 
-cat ./wallpaper/all.txt | cut -d ' ' -f 1|perl -pe 's/(\d{4})\/(\d{2}).*/$1-$2/'|sort -rn|uniq|xargs -n1 sh -c 'echo "# $0" > "./archive/$0.md"; echo "" >> "./archive/$0.md"'
-cat ./wallpaper/all.txt | cut -d ' ' -f 1|perl -pe 'print $_; s/(\d{4})\/(\d{2}).*/$1-$2/'|xargs -n2 sh -c 'cat "./wallpaper/$0"|sed "s/^#/##/" >> "./archive/$1.md"'
+cat ./wallpaper/all.txt | cut -d ' ' -f 1|perl -pe 's/(\d{4})\/(\d{2}).*/$1-$2/'|sort -rn|uniq|tr '-' '\n'|xargs -n2 sh -c 'mkdir -p "./archive/$0"; echo "# $0-$1" > "./archive/$0/$1.md"; echo "" >> "./archive/$0/$1.md"'
+cat ./wallpaper/all.txt | cut -d ' ' -f 1|perl -pe 'print $_; s/(\d{4})\/(\d{2}).*/$1-$2/'|tr '-' '\n'|xargs -n3 sh -c 'cat "./wallpaper/$0"|sed "s/^#/##/" >> "./archive/$1/$2.md"'
 
 echo "# Archives" >> README.md
 echo "" >> README.md
 
-ls ./archive/*.md|sort -rn|perl -pe 'print $_; s/.*(\d{4}-\d{2}).*/$1/'|xargs -n2 sh -c 'echo "[$1]($0)" >> README.md; echo "" >> README.md'
+find ./archive/ -iname '*.md'|sort -rn|perl -pe 'print $_; s/.*(\d{4})\/(\d{2}).*/$1-$2/'|xargs -n2 sh -c 'echo "[$1]($0)" >> README.md; echo "" >> README.md'
 
