f4eb749789faf126b763ff363df0717829fd8240
OpenGG <liy099@gmail.com>
2023-03-22 04:14:57 +0000

    feat: add retry

--- scripts/cron.ts
+++ scripts/cron.ts
@@ -1,31 +1,13 @@
-import type { BingImage } from './lib/BingImage.ts'
-import { isValidBingImage } from './lib/BingImage.ts'
+import { getBingImages } from './lib/BingImage.ts'
 import { output } from "./lib/output.ts";
 import { processUrl } from './lib/processUrl.ts'
 import { resolve } from './deps/path.ts'
-
+import { retry } from './lib/retry.ts'
 
 const main = async () => {
     const root = resolve('wallpaper')
 
-    const api = 'https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&uhd=1&uhdwidth=3840&uhdheight=2160&mkt=en-US'
-    const res = await fetch(api)
-
-    if (res.status !== 200) {
-        throw new Error(`Response status not valid (${res.status}): ${api}`)
-    }
-
-    const json = await res.json()
-
-    const images = json?.images as BingImage[]
-
-    if (!images || !Array.isArray(images)) {
-        throw new Error(`Invalid response: ${api}`)
-    }
-
-    if (!images.every(isValidBingImage)) {
-        throw new Error(`Invalid response content: ${api}`)
-    }
+    const images = await retry(getBingImages);
 
     for (const {
         startdate,
--- scripts/lib/BingImage.ts
+++ scripts/lib/BingImage.ts
@@ -1,11 +1,11 @@
-export type BingImage = {
+type BingImage = {
     startdate: string
     url: string
     copyright: string
     title: string
 }
 
-export const isValidBingImage = ({
+const isValidBingImage = ({
     startdate,
     url,
     copyright,
@@ -17,3 +17,26 @@ export const isValidBingImage = ({
     && typeof copyright === 'string'
     && typeof title === 'string'
 )
+
+export const getBingImages = async (): Promise<BingImage[]> => {
+    const api = 'https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&uhd=1&uhdwidth=3840&uhdheight=2160&mkt=en-US'
+    const res = await fetch(api)
+    
+    if (res.status !== 200) {
+        throw new Error(`Response status not valid (${res.status}): ${api}`)
+    }
+    
+    const json = await res.json()
+    
+    const images = json?.images as BingImage[]
+    
+    if (!images || !Array.isArray(images)) {
+        throw new Error(`Invalid response: ${api}`)
+    }
+    
+    if (!images.every(isValidBingImage)) {
+        throw new Error(`Invalid response content: ${api}`)
+    }
+
+    return images;
+}
