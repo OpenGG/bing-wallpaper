e15aa8350a5444fd16b548ee240b0451a71d1714
OpenGG <liy099@gmail.com>
2025-07-25 18:31:29 +0000

    refactor: 将壁纸列表文件路径从 wallpaper 目录调整至 archive 目录

--- app/src/lib/config.ts
+++ app/src/lib/config.ts
@@ -5,7 +5,7 @@ export const DIR_ARCHIVE = "archive";
 export const ALL_TXT = "all.txt";
 export const CURRENT_TXT = "current.txt";
 
-export const PATH_ALL_TXT = join(DIR_WALLPAPER, ALL_TXT);
-export const PATH_CURRENT_TXT = join(DIR_WALLPAPER, CURRENT_TXT);
+export const PATH_ALL_TXT = join(DIR_ARCHIVE, ALL_TXT);
+export const PATH_CURRENT_TXT = join(DIR_ARCHIVE, CURRENT_TXT);
 
 export const PATH_README = "./README.md";
